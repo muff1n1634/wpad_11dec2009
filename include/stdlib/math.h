03bf75eac6a4c4945dec55e9e58bc7b882058a5e
muff1nOS <19197077+muff1n1634@users.noreply.github.com>
2024-08-13 11:31:53 -0700

    clean up headers, mostly `macros.h`

--- include/stdlib/math.h
+++ include/stdlib/math.h
@@ -1,10 +1,10 @@
 #ifndef __STDC_MATH_H__
 #define __STDC_MATH_H__
 
-double atan(double x);
+extern double atan(double x);
 
-double cos(double x);
-double sin(double x);
-double tan(double x);
+extern double cos(double x);
+extern double sin(double x);
+extern double tan(double x);
 
 #endif // __STDC_MATH_H__
--- include/types.h
+++ include/types.h
@@ -7,34 +7,34 @@
 // NULL
 
 #undef NULL
-#define NULL				0
+#define NULL			0
 
 // Fixed-width types
 
-typedef uint8_t				u8;
-typedef uint16_t			u16;
-typedef uint32_t			u32;
-typedef uint64_t			u64;
+typedef uint8_t			u8;
+typedef uint16_t		u16;
+typedef uint32_t		u32;
+typedef uint64_t		u64;
 
-typedef  int8_t				s8;
-typedef  int16_t			s16;
-typedef  int32_t			s32;
-typedef  int64_t			s64;
+typedef  int8_t			s8;
+typedef  int16_t		s16;
+typedef  int32_t		s32;
+typedef  int64_t		s64;
 
 // Floating-point types
 
-typedef float				f32;
-typedef double				f64;
+typedef float			f32;
+typedef double			f64;
 
 // Booleans
 
-typedef int					BOOL;
-#define TRUE				1
-#define FALSE				0
+typedef int				BOOL;
+#define TRUE			1
+#define FALSE			0
 
 // other
 
-typedef unsigned char		byte_t;
-typedef long unsigned int	register_t;
+typedef unsigned char	byte_t;
+typedef unsigned int	register_t;
 
 #endif // TYPES_H
